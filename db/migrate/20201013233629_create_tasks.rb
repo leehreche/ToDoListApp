d445573eadbc9871f8304d65fab7f27a70d46bbc
Letícia de Almeida Reche <leticia.reche@hotmail.com>
2020-10-14 01:07:21 -0300

    Tarefas da Atividade 04 da disciplina de Projeto de Software

---
+++ db/migrate/20201013233629_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.date :due
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
